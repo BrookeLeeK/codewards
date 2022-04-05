4c0b977fdccaf203dcd49f0c4187b040393267d6
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-04-04 20:22:29 -0500

    codewars solution 04.04.2022

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Write a program that finds the summation of every number from 1 to num. 
+// The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+var summation = function (num) {
+    let arr = []
+  //first get an array of 1 to the number
+  for(let i = 1; i <= num; i++){
+      arr.push(i);
+  }
+  //now use reduce to add the whole array together
+let newArr = arr.reduce( (acc, curr) => acc +=curr, 0)
+return newArr
+}
+
+summation()
