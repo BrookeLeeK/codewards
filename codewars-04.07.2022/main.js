f89584bbeaf7da984da34be7909d0a0e5ddb13fc
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-04-07 20:41:25 -0500

    codewars for April 7th 2022

---
+++ codewars-04.07.2022/main.js
@@ -0,0 +1,37 @@
+//You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+function positiveSum(arr) {
+	let total = 0;
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] >0){
+      total += arr[i];
+    }
+  }
+  return total
+}
+
+positiveSum([1, -3, 4])
+
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+    if(num > 0){
+      return num*=-1
+    }else{
+      return num
+    }
+  }
