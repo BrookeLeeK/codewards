d3b0cddb86b88a03d664e5f683e54126c7d0e9da
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-04-29 23:24:26 -0500

    added one 6KYU

--- codewars-04.29.2022/main.js
+++ codewars-04.29.2022/main.js
@@ -5,3 +5,20 @@ function highAndLow(numbers){
     let sorted = arr.sort((a, b) => a-b)
     return `${sorted[sorted.length-1]} ${sorted[0]}`
     }
+
+
+//     Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+    let arr = string.split(' ')
+    let newArr = arr.map( x=> {
+      if(x.split('').length>4){
+        return x.split('').reverse().join('')
+      }else{
+        return x
+      }
+    })
+    return newArr.join(' ')
+  }
