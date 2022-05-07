5975200c24dbc2ab8ad8191dae79b9724fed9be3
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-05-06 22:16:51 -0500

    codewars for May 6th 2022

---
+++ codewars-05.06.2022/main.js
@@ -0,0 +1,21 @@
+//Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+var isSquare = function(n){
+    if((Math.sqrt(n))%1==0){
+      return true
+    }else{
+      return false
+    }
+  }
+
+//   Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
