a6aac372a322b183ed52231bc7c3112ec9a52a18
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-05-12 23:14:05 -0500

    codewars first challenge for May 12th

---
+++ codewars-05.12.2022.challenge1/main.js
@@ -0,0 +1,14 @@
+//Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+//return the total of all three leagues
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+  
+  // goals(43, 10, 0) // should return 53
