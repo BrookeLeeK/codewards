a11b19fee1ef376faa6c30bf90d26020c07dc223
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-05-13 23:13:42 -0500

    codewars May 13 2022

--- codewars-04.23.2022/main.js
+++ codewars-04.23.2022/main.js
@@ -1,14 +1,19 @@
-// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// The Story:
+// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
 
-// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// Task Overview:
+// You have to write a function that accepts three parameters:
 
-// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 
-// Examples:
 
-var number=  function(array){
-    let newArr = array.map( x =>{
-      return `${array.indexOf(x)+1}: ${x}`
-    })
-    return newArr
+function enough(cap, on, wait) {
+    if(wait <= cap-on){
+      return 0
+    }else{
+      return wait-(cap-on)
+    }
   }
