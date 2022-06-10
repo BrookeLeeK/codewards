4746e23f49afa17cc440b8cce37c073f338f48b0
BrookeLeeK <kalmanbrooke@yahoo.com>
2022-06-10 18:40:22 -0500

    first challenge for June 10th 2022

---
+++ codewars-06.10.2022/firstProb/main.js
@@ -0,0 +1,15 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+
+function square(num){
+    return num * num
+  }
+  
+  square(3)  //should return 9 
+  
+  
+  
+  //P - parameter is a number
+  //R - return a number
+  //E - if num is 2, return 4
+  //P - return num*num
